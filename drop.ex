e3d3e0d05419e8d214f6a57a4fe19f94bb70d767
Juraci Vieira <juraci.vieira@gmail.com>
2016-04-10 14:37:44 -0300

    Adding amazing documentation

--- drop.ex
+++ drop.ex
@@ -1,4 +1,18 @@
 defmodule Drop do
+    @moduledoc """
+    Functions calculating velocities achieved by objects dropped on a vacuum.
+    
+    from *Introducing Elixir*
+    """
+
+    @doc """
+    Calculates the velocity of an object falling on Earth
+    as if it were in a vacuum (no air resistance). The distance is
+    the height from which the object falls, specified in meters,
+    and the function returns a velocity in meters per second.
+    """
+
+    @spec fall_velocity(number()) :: number()
 
     def fall_velocity(distance) do
         import :math, only: [sqrt: 1]
